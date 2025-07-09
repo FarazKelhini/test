29ba444483ff6734f168fe340b42e194ba7ddc42
FarazKelhini <farazkelhini@gmail.com>
2025-07-09 14:19:54 +0330

    Update tests/example.spec

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,12 @@
-import { expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-await expect(async () => {
-  await page.getByText("Login").click();
-}).toPass({
-  timeout: 3000,
-  interval: 1000,
+test('Interact with iframe', async ({ page }) => {
+  await page.goto('https://testpages.eviltester.com/styled/iframes-test.html');
+
+  // Locate the iframe
+  const frame = page.frameLocator('#thedynamichtml');
+
+  // Interact with elements inside the iframe
+  await expect(frame.getByRole('heading', { name: 'iFrame' }))
+    .toBeVisible();
 });
